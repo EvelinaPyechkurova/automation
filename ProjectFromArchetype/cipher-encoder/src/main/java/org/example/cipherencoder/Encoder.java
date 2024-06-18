ff4cf75cc80aa677ca0d7b1b72eaa5718a183949
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2024-06-18 15:03:39 +0300

    Created profile, archetype and project based on archetype

---
+++ ProjectFromArchetype/cipher-encoder/src/main/java/org/example/cipherencoder/Encoder.java
@@ -0,0 +1,18 @@
+package org.example.cipherencoder;
+
+public class Encoder {
+    public static String encode(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+
+        for (char character : text.toCharArray()) {
+            if (Character.isLetter(character)) {
+                char base = Character.isLowerCase(character) ? 'a' : 'A';
+                result.append((char) ((character - base + shift) % 26 + base));
+            } else {
+                result.append(character);
+            }
+        }
+
+        return result.toString();
+    }
+}
